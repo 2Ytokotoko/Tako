3005c6ed3303287f2dabf964b844e0127ae75122
KPC-020C1380\m0a2034140 <M0A2034140@edu.teu.ac.jp>
2022-07-21 09:12:22 +0900

    ステージ2お題

--- Assets/Scripts/Screen/Odai2.cs
+++ Assets/Scripts/Screen/Odai2.cs
@@ -5,40 +5,32 @@ using UnityEngine.UI;
 
 public class Odai2 : MonoBehaviour
 {
-    public GameObject Main1, Main2, Main3, Main4, Clear1, Clear2, Clear3, Clear4;
-    public int pScore, kScore;
-    public float Count;
-    public int MIITOBOURUCOUNT, NAPORITANNCOUNT;
+    public GameObject Main1, Main2, Main3,Main4, Clear1, Clear2, Clear3,Clear4;
+    public int G, Kal, En;
+    public float Ime;
+    public int wScore, NAPORITANNCOUNT, MIITOBOURUCOUNT,COUNT, Gcheck, Wcheck,Pcheck;
+    public CountDown countDown;
+    public Weight weight;
+    private AudioSource audioSource;
+    public AudioClip se1, se2;
+    Kcal kal;
+    Weight g;
+    Price en;　//緑波線出てるかもだけど問題なく反映されてた
     CountDown time;
 
-    //値段
-    public int pKARAAGE = 70;
-    public int pHANNBAAGU = 100;
-    public int pTOMATO = 5;
-    public int pTAMAGOYAKI = 20;
-    public int pBUROKKORII = 10;
-    public int pTAKO = 20;
-    public int pSYUUMAI = 15;
-    public int pMIITOBOURU = 20;
-    public int pNAPORITANN = 65;
-    public int pPASUTA = 60;
-
-    //カロリー
-    public int kKARAAGE = 70;
-    public int kHANNBAAGU = 130;
-    public int kTOMATO = 5;
-    public int kTAMAGOYAKI = 20;
-    public int kBUROKKORII = 10;
-    public int kTAKO = 20;
-    public int kSYUUMAI = 25;
-    public int kMIITOBOURU = 25;
-    public int kNAPORITANN = 50;
-    public int kPASUTA = 30;
-
-    public GameObject BentoName;
+    //スクリプトが入っているオブジェクト
+    GameObject KS;
+    GameObject TT;
+    private GameObject Bentoname;
 
     void Start()
     {
+        TT = GameObject.Find("Time");
+        KS = GameObject.Find("Hantei");
+        kal = KS.GetComponent<Kcal>();
+        g = KS.GetComponent<Weight>();
+        en = KS.GetComponent<Price>();
+        time = TT.GetComponent<CountDown>();
         Main1.SetActive(true);
         Clear1.SetActive(false);
         Main2.SetActive(true);
@@ -47,89 +39,154 @@ public class Odai2 : MonoBehaviour
         Clear3.SetActive(false);
         Main4.SetActive(true);
         Clear4.SetActive(false);
-        NAPORITANNCOUNT = 0;
-        MIITOBOURUCOUNT = 0;
-        pScore = 20;
-        kScore = 235;
         Check();
-        time = FindObjectOfType<CountDown>();
     }
     void Update()
     {
-        Count = time.countdown;
+        Kal = kal.score;
+        G = g.score;
+        En = en.score;
+        Ime = time.countdown;
+        Bentoname = GameObject.Find("OdaiName");
         Check();
     }
     void OnTriggerEnter(Collider other)
     {
         //個数カウント
-        if (other.CompareTag("KARAAGE"))
-        {
-            kScore += kKARAAGE;
-            pScore += pKARAAGE;
-        }
-        if (other.CompareTag("HANNBAAGU"))
-        {
-            kScore += kHANNBAAGU;
-            pScore += pHANNBAAGU;
-        }
-        //サブ
-        if (other.CompareTag("TOMATO"))
-        {
-            kScore += kTOMATO;
-            pScore += pTOMATO;
-        }
-        if (other.CompareTag("TAMAGOYAKI"))
-        {
-            kScore += kTAMAGOYAKI;
-            pScore += pTAMAGOYAKI;
-        }
-        if (other.CompareTag("TAKO") || other.CompareTag("Player"))
-        {
-            kScore += kTAKO;
-            pScore += pTAKO;
-        }
-        if (other.CompareTag("SYUUMAI"))
-        {
-
-            kScore += kSYUUMAI;
-            pScore += pSYUUMAI;
-        }
-        if (other.CompareTag("MIITOBOURU"))
-        {
-            MIITOBOURUCOUNT = MIITOBOURUCOUNT + 1;
-            kScore += kMIITOBOURU;
-            pScore += pMIITOBOURU;
-        }
-        //サブ
         if (other.CompareTag("NAPORITANN"))
         {
-            NAPORITANNCOUNT = NAPORITANNCOUNT + 1;
-            kScore += kNAPORITANN;
-            pScore += pNAPORITANN;
+            NAPORITANNCOUNT += 1;
         }
-        if (other.CompareTag("PASUTA"))
+        Check();
+        //個数カウント
+        if (other.CompareTag("MIITOBOURU"))
         {
-            kScore += kPASUTA;
-            pScore += pPASUTA;
+            MIITOBOURUCOUNT += 1;
+        }
+        Check();
+    }
+    void OnTriggerExit(Collider other)
+    {
+        //個数カウント
+        if (other.CompareTag("datuNAPORITANN"))
+        {
+            NAPORITANNCOUNT -= 1;
+        }
+        Check();
+        //個数カウント
+        if (other.CompareTag("datuMIITOBOURU"))
+        {
+            MIITOBOURUCOUNT -= 1;
         }
         Check();
     }
 
     void Check()
     {
-        //お題①ナポリタン4個以上、ミートボール6個以上
+        //お題①ナポリタンをを4個以上&ミートボールをを6個以上
         if ((NAPORITANNCOUNT >= 4) && (MIITOBOURUCOUNT >= 6))
         {
             Main1.SetActive(false);
             Clear1.SetActive(true);
-
+            audioSource = gameObject.GetComponent<AudioSource>();
+            audioSource.clip = se1;
+            audioSource.Play();
+            Gcheck = 1;
         }
-        else if ((NAPORITANNCOUNT != 4) || (MIITOBOURUCOUNT != 6))
+        else if (Gcheck == 1)
+        {
+            Main1.SetActive(true);
+            Clear1.SetActive(false);
+            audioSource = gameObject.GetComponent<AudioSource>();
+            audioSource.clip = se2;
+            audioSource.Play();
+        }
+        else
         {
             Main1.SetActive(true);
             Clear1.SetActive(false);
 
         }
+
+        //お題②カロリー500以上
+        if (Kal >= 500)
+        {
+            Main2.SetActive(false);
+            Clear2.SetActive(true);
+            audioSource = gameObject.GetComponent<AudioSource>();
+            audioSource.clip = se1;
+            audioSource.Play();
+            Wcheck = 1;
+        }
+        else if (Wcheck == 1)
+        {
+            Main2.SetActive(true);
+            Clear2.SetActive(false);
+            audioSource = gameObject.GetComponent<AudioSource>();
+            audioSource.clip = se2;
+            audioSource.Play();
+        }
+        else
+        {
+            Main2.SetActive(true);
+            Clear2.SetActive(false);
+
+        }
+
+        //お題③5分以内にクリア
+        if (Ime <= 300)
+        {
+            if (Bentoname != null)
+            {
+                Main3.SetActive(false);
+                Clear3.SetActive(true);
+                audioSource = gameObject.GetComponent<AudioSource>();
+                audioSource.clip = se1;
+                audioSource.Play();
+            }
+            else
+            {
+                Main3.SetActive(true);
+                Clear3.SetActive(false);
+            }
+        }
+        //お題④値段1000以下
+        if (En <= 1000)
+        {
+            Main4.SetActive(true);
+            Clear4.SetActive(false); audioSource = gameObject.GetComponent<AudioSource>();
+            audioSource.clip = se1;
+            audioSource.Play();
+            Pcheck = 1;
+        }
+        else if (Pcheck == 1)
+        {
+            Main4.SetActive(true);
+            Clear4.SetActive(false);
+            audioSource = gameObject.GetComponent<AudioSource>();
+            audioSource.clip = se2;
+            audioSource.Play();
+        }
+        else
+        {
+            Main4.SetActive(true);
+            Clear4.SetActive(false);
+
+        }
+    }
+}
+
+/*
+
+void Check()
+{
+    //お題①ナポリタン4個以上、ミートボール6個以上
+    if ((NAPORITANNCOUNT >= 4) && (MIITOBOURUCOUNT >= 6))
+    {
+            Main1.SetActive(false);
+            Clear1.SetActive(true);
+
+    }
         //お題②カロリー500以上
         if (kScore >= 500)
         {
@@ -137,32 +194,20 @@ public class Odai2 : MonoBehaviour
             Clear2.SetActive(true);
         }
 
-        else if(kScore < 500)
-        {
-            Main2.SetActive(true);
-            Clear2.SetActive(false);
-        }
-
-        //お題③値段1000以下
-        if (pScore <= 1000)
-        {
-            Main3.SetActive(false);
-            Clear3.SetActive(true);
-        }
-        else if(pScore > 1000)
-        {
-            Main3.SetActive(true);
-            Clear3.SetActive(false);
-        }
-
-        //お題④5分以内にクリア
+        //お題③5分以内にクリア
         if (Count <= 300)
         {
-            if (BentoName.activeSelf)
+            if (Time.timeScale == 0f)
             {
-                Main4.SetActive(false);
-                Clear4.SetActive(true);
+                Main3.SetActive(false);
+                Clear3.SetActive(true);
+            }
+            else
+            {
+                Main3.SetActive(true);
+                Clear3.SetActive(false);
             }
         }
+
     }
-}
+}*/
--- Assets/Scripts/Screen/Odai3.cs
+++ Assets/Scripts/Screen/Odai3.cs
@@ -5,53 +5,32 @@ using UnityEngine.UI;
 
 public class Odai3 : MonoBehaviour
 {
-    public GameObject Main1, Main2, Main3, Main4, Main5, Clear1, Clear2, Clear3, Clear4, Clear5;
-    public int pScore, kScore,wScore;
-    public float Count;
-    public int EBIHURAICOUNT;
+    public GameObject Main1, Main2, Main3,Main4,Main5,Clear1, Clear2, Clear3,Clear4,Clear5;
+    public int G, Kal, En;
+    public float Ime;
+    public int wScore, EBIHURAICOUNT,COUNT, Gcheck, Wcheck,Pcheck;
     public CountDown countDown;
+    public Weight weight;
+    private AudioSource audioSource;
+    public AudioClip se1, se2;
+    Kcal kal;
+    Weight g;
+    Price en;　//緑波線出てるかもだけど問題なく反映されてた
+    CountDown time;
 
-    //値段
-    public int pKARAAGE = 70;
-    public int pHANNBAAGU = 100;
-    public int pTOMATO = 5;
-    public int pTAMAGOYAKI = 20;
-    public int pBUROKKORII = 10;
-    public int pTAKO = 20;
-    public int pSYUUMAI = 15;
-    public int pMIITOBOURU = 20;
-    public int pNAPORITANN = 65;
-    public int pPASUTA = 60;
-    public int pEBIHURAI = 100;
-
-    //カロリー
-    public int kKARAAGE = 70;
-    public int kHANNBAAGU = 130;
-    public int kTOMATO = 5;
-    public int kTAMAGOYAKI = 20;
-    public int kBUROKKORII = 10;
-    public int kTAKO = 20;
-    public int kSYUUMAI = 25;
-    public int kMIITOBOURU = 25;
-    public int kNAPORITANN = 50;
-    public int kPASUTA = 30;
-    public int kEBIHURAI = 100;
-    
-    //重量
-    public int wKARAAGE = 30;
-    public int wEBIHURAI = 30;
-    public int wTOMATO = 10;
-    public int wTAMAGOYAKI = 40;
-    public int wBUROKKORII = 20;
-    public int wTAKO = 20;
-    public int wSYUUMAI = 15;
-    public int wMIITOBOURU = 10;
-    public int wNAPORITANN = 35;
-    public int wPASUTA = 35;
-    public int wHANNBAAGU = 70;
+    //スクリプトが入っているオブジェクト
+    GameObject KS;
+    GameObject TT;
+    private GameObject Bentoname;
 
     void Start()
     {
+        TT = GameObject.Find("Time");
+        KS = GameObject.Find("Hantei");
+        kal = KS.GetComponent<Kcal>();
+        g = KS.GetComponent<Weight>();
+        en = KS.GetComponent<Price>();
+        time = TT.GetComponent<CountDown>();
         Main1.SetActive(true);
         Clear1.SetActive(false);
         Main2.SetActive(true);
@@ -62,135 +41,153 @@ public class Odai3 : MonoBehaviour
         Clear4.SetActive(false);
         Main5.SetActive(true);
         Clear5.SetActive(false);
-        EBIHURAICOUNT = 0;
-        pScore = 20;
-        kScore = 235;
-        wScore = 150;
         Check();
     }
     void Update()
     {
-        Count = countDown.countdown;
+        Kal = kal.score;
+        G = g.score;
+        En = en.score;
+        Ime = time.countdown;
+        Bentoname = GameObject.Find("OdaiName");
         Check();
     }
     void OnTriggerEnter(Collider other)
     {
         //個数カウント
-        if (other.CompareTag("KARAAGE"))
-        {
-            kScore += kKARAAGE;
-            pScore += pKARAAGE;
-            wScore += wKARAAGE;
-        }
-        if (other.CompareTag("HANNBAAGU"))
-        {
-            kScore += kHANNBAAGU;
-            pScore += pHANNBAAGU;
-            wScore += wHANNBAAGU;
-        }
-        //サブ
-        if (other.CompareTag("TOMATO"))
-        {
-            kScore += kTOMATO;
-            pScore += pTOMATO;
-            wScore += wTOMATO;
-        }
-        if (other.CompareTag("TAMAGOYAKI"))
-        {
-            kScore += kTAMAGOYAKI;
-            pScore += pTAMAGOYAKI;
-            wScore += wTAMAGOYAKI;
-        }
-        if (other.CompareTag("TAKO") || other.CompareTag("Player"))
-        {
-            kScore += kTAKO;
-            pScore += pTAKO;
-            wScore += wTAKO;
-        }
-        if (other.CompareTag("SYUUMAI"))
-        {
-
-            kScore += kSYUUMAI;
-            pScore += pSYUUMAI;
-            wScore += wSYUUMAI;
-        }
-        if (other.CompareTag("MIITOBOURU"))
-        {
-            kScore += kMIITOBOURU;
-            pScore += pMIITOBOURU;
-            wScore += wMIITOBOURU;
-        }
-        //サブ
-        if (other.CompareTag("NAPORITANN"))
-        {
-            kScore += kNAPORITANN;
-            pScore += pNAPORITANN;
-            wScore += wNAPORITANN;
-        }
-        if (other.CompareTag("PASUTA"))
-        {
-            kScore += kPASUTA;
-            pScore += pPASUTA;
-            wScore += wPASUTA;
-        }
         if (other.CompareTag("EBIHURAI"))
         {
-            EBIHURAICOUNT = EBIHURAICOUNT + 1;
-            kScore += kEBIHURAI;
-            pScore += pEBIHURAI;
-            wScore += wEBIHURAI;
+            EBIHURAICOUNT += 1;
+
+            Check();
+
+        }
+    }
+    void OnTriggerExit(Collider other)
+    {
+        //個数カウント
+        if (other.CompareTag("datuEBIHURAI"))
+        {
+                EBIHURAICOUNT -= 1;
         }
         Check();
     }
 
     void Check()
     {
+
+
+
+
+
+  
+            
         //お題①エビフライ3個以上
-        if (EBIHURAICOUNT >= 3)
+            if (EBIHURAICOUNT >= 3)
         {
-                Main1.SetActive(false);
-                Clear1.SetActive(true);
-        }        
+            Main1.SetActive(false);
+            Clear1.SetActive(true);
+            audioSource = gameObject.GetComponent<AudioSource>();
+            audioSource.clip = se1;
+            audioSource.Play();
+            Gcheck = 1;
+        }
+        else if (Gcheck == 1)
+        {
+            Main1.SetActive(true);
+            Clear1.SetActive(false);
+            audioSource = gameObject.GetComponent<AudioSource>();
+            audioSource.clip = se2;
+            audioSource.Play();
+        }
+        else
+        {
+            Main1.SetActive(true);
+            Clear1.SetActive(false);
+
+        }
+
         //お題②重量400以上
-        if (wScore >= 400)
+        if (G >= 400)
         {
             Main2.SetActive(false);
             Clear2.SetActive(true);
+            audioSource = gameObject.GetComponent<AudioSource>();
+            audioSource.clip = se1;
+            audioSource.Play();
+            Wcheck = 1;
+        }
+        else if (Wcheck == 1)
+        {
+            Main2.SetActive(true);
+            Clear2.SetActive(false);
+            audioSource = gameObject.GetComponent<AudioSource>();
+            audioSource.clip = se2;
+            audioSource.Play();
+        }
+        else
+        {
+            Main2.SetActive(true);
+            Clear2.SetActive(false);
+
         }
         //お題③カロリー400以上
-        if (kScore >= 400)
+        if (Kal >= 400)
         {
-            Main3.SetActive(false);
-            Clear3.SetActive(true);
+            Main3.SetActive(true);
+            Clear3.SetActive(false); audioSource = gameObject.GetComponent<AudioSource>();
+            audioSource.clip = se1;
+            audioSource.Play();
+            Pcheck = 1;
+        }
+        else if (Pcheck == 1)
+        {
+            Main3.SetActive(true);
+            Clear3.SetActive(false);
+            audioSource = gameObject.GetComponent<AudioSource>();
+            audioSource.clip = se2;
+            audioSource.Play();
+        }
+        else
+        {
+            Main3.SetActive(true);
+            Clear3.SetActive(false);
+
+        }
+
+
+        //お題⑤7分以内にクリア
+        if (Ime <= 420)
+        {
+            if (Bentoname != null)
+            {
+                Main5.SetActive(false);
+                Clear5.SetActive(true);
+                audioSource = gameObject.GetComponent<AudioSource>();
+                audioSource.clip = se1;
+                audioSource.Play();
+            }
+            else
+            {
+                Main5.SetActive(true);
+                Clear5.SetActive(false);
+            }
         }
         //お題④値段600以下
-        if (kScore >= 600)
+        if (En <= 600)
         {
             Main4.SetActive(true);
             Clear4.SetActive(false);
         }
         else
         {
-            Main4.SetActive(false);
-            Clear4.SetActive(true);
-
-
-
-            //お題⑤5分以内にクリア
-            if (Count <= 300)
-        {
-            if (Time.timeScale == 0f)
-            {
-                Main3.SetActive(false);
-                Clear3.SetActive(true);
-            }
-            else
-            {
-                Main3.SetActive(true);
-                Clear3.SetActive(false);
-            }
-        }
+            Main4.SetActive(true);
+            Clear4.SetActive(false);
+            audioSource = gameObject.GetComponent<AudioSource>();
+            audioSource.clip = se2;
+            audioSource.Play();
 
         }
     }
 }
+
